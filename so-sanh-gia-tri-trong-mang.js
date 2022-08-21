41c1da5d2dccc528e7a8b2da445560c422e4eac7
Lam <lnhlam99@gmail.com>
2022-08-21 22:01:49 +0700

    fearture-03-fearture

--- so-sanh-gia-tri-trong-mang.js
+++ so-sanh-gia-tri-trong-mang.js
@@ -1,8 +1,14 @@
 let test = [1,2,3,4,5,10,6,7,8,9];
 let i;
 let max = 0;
+let count= 0;
+let position;
+
 for (i = 0;i < test.length;i++) {
-    if (test[i] > max)
-        max = test[i]
+    if (test[i] > max) {
+        max = test[i];
+        position = count;
+    }
+    count++;
 }
-console.log(max)
+console.log(max + " có giá trị lớn nhát ở vị trí thứ " + position);
